2e7cc398416ded4d30be31b425f455b390844526
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-07-18 15:40:01 +0530

    Paste URL works

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,16 @@
-function promalert() {
-    swal({
-        title: "Add New Videos",
-        text: "Enter video URLs in seperate lines",
-        type: "input",
-        showCancelButton: true,
-        closeOnConfirm: false,
-        animation: "slide-from-top",
-        inputPlaceholder: "URLs..."
-    },
+var URL_TEXT = false;
 
+function inputVideoURLs() {
+    swal(
+        {
+            title: "Add New Videos",
+            text: "Enter video URLs in seperate lines",
+            type: "input",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            animation: "slide-from-top",
+            inputPlaceholder: "URLs..."
+        },
         function (inputValue) {
             if (inputValue === false)
                 return false;
@@ -16,15 +18,43 @@ function promalert() {
                 swal.showInputError("Please enter a URL");
                 return false;
             }
+            URL_TEXT = inputValue
             urlCount = inputValue.split(/\r?\n/);
-
             if (urlCount.length == 1) {
                 correct_tense = " URL"
             } else {
                 correct_tense = " URLs"
             }
             swal("Video URLs", "You entered " + urlCount.length + correct_tense, "success");
-        });
+        }
+    );
+
+}
+
+function pasteURLs() {
+    try {
+        navigator.clipboard.read()
+            .then(text => {
+                urlCount = text.split(/\r?\n/);
+                if (urlCount.length == 1) {
+                    correct_tense = " URL"
+                } else {
+                    correct_tense = " URLs"
+                }
+                swal("Video URLs", "You entered " + urlCount.length + correct_tense, "success");
+                URL_TEXT = text;
+                return text;
+            })
+            .catch(err => {
+                swal("an Error has occured", err, "error");
+                return false;
+            });
+    } catch (e) {
+        if (e instanceof TypeError) {
+            swal("Clipboard Access Blocked", e, "error");
+        }
+        return false;
+    }
 }
 
 $(document).ready(function () {
@@ -32,14 +62,16 @@ $(document).ready(function () {
     // enter link
     $(document).on("click", "a#addNewVideoButton, a#addNewVideoDropdown",
         function () {
-            promalert();
+            inputVideoURLs();
+            console.log(URL_TEXT)
         }
     )
 
     // paste link
-    $(document).on("click", "",
+    $(document).on("click", "a#pasteVideoLinkButton, a#pasteVideoLinkDropdown",
         function () {
-            return true;
+            pasteURLs();
+            console.log(URL_TEXT);
         }
     )
 
