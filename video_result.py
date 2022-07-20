f2e8b93e900c57a3b06c2e7eb9e86ac6e9619291
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-07-20 12:10:10 +0530

    youtube result

---
+++ video_result.py
@@ -0,0 +1,47 @@
+data = {
+    'type': 'video',
+    'id': 'SA7AIQw-7Ms',
+    'title': 'G-Eazy & Halsey - Him & I (Official Video)',
+    'publishedTime': '4 years ago',
+    'duration': '4: 47',
+    'viewCount': {
+            'text': '594, 670, 331 views',
+            'short': '594M views'
+    },
+    'thumbnails': [
+        {
+            'url': 'https: //i.ytimg.com/vi/SA7AIQw-7Ms/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDdBpd-75UnGAaC4auJ2zR_4mI_VA',
+            'width': 360,
+            'height': 202
+        },
+        {
+            'url': 'https://i.ytimg.com/vi/SA7AIQw-7Ms/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAYRNkY2zTsMOCP7L7u5YNbmQHUzg',
+            'width': 720,
+            'height': 404
+        }
+    ],
+    'richThumbnail': None,
+    'descriptionSnippet': [
+        {
+            'text': "New Album 'The Beautiful & Damned' Available Everywhere http://smarturl.it/TBAD Get exclusive TB&D bundles here:\xa0..."
+        }
+    ],
+    'channel': {
+        'name': 'G-Eazy',
+        'id': 'UCBkNpeyvBO2TdPGVC_PsPUA',
+        'thumbnails': [
+                {
+                    'url': 'https://yt3.ggpht.com/GDvg8-AlnhdsRKFVWFjPSMvSmFht-hQknhgByLaIw4z6n8YYzHZaPi48f2-dlG42kOSCbDljBFk=s88-c-k-c0x00ffffff-no-rj',
+                    'width': 68,
+                    'height': 68
+                }
+        ],
+        'link': 'https://www.youtube.com/channel/UCBkNpeyvBO2TdPGVC_PsPUA'
+    },
+    'accessibility': {
+        'title': 'G-Eazy & Halsey - Him & I (Official Video) by G-Eazy 4 years ago 4 minutes, 47 seconds 594,670,331 views',
+        'duration': '4 minutes, 47 seconds'
+    },
+    'link': 'https://www.youtube.com/watch?v=SA7AIQw-7Ms',
+    'shelfTitle': None
+}
